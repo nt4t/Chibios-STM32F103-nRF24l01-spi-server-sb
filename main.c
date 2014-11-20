86b463eb14ea569409ef53532c04a3b9ae4c3623
nt4t <nt4ttt@gmail.com>
2014-11-20 10:43:47 +0700

    update

--- main.c
+++ main.c
@@ -11,10 +11,10 @@ Chibios-STM32F103-nRF24l01-spi-server-sb
  * Low speed SPI configuration (140.625kHz, CPHA=0, CPOL=0, MSb first).
  */
 static const SPIConfig ls_spicfg = {
-  NULL,
-  GPIOB,
-  GPIOB_SPI2NSS,
-  SPI_CR1_BR_2 | SPI_CR1_BR_1
+    NULL,
+    GPIOB,
+    GPIOB_SPI2NSS,
+    SPI_CR1_BR_2 | SPI_CR1_BR_1
 };
 
 /*
@@ -24,53 +24,48 @@ static const SPIConfig ls_spicfg = {
 static WORKING_AREA(blinker_wa, 128);
 static msg_t blinker(void *arg) {
 
-  (void)arg;
-  chRegSetThreadName("blinker");
-  while (TRUE) {
+    (void)arg;
+    chRegSetThreadName("blinker");
+    while (TRUE) {
 //    palSetPad(GPIOC, GPIOC_LED3);
-    chThdSleepMilliseconds(500);
+        chThdSleepMilliseconds(500);
 //    palClearPad(GPIOC, GPIOC_LED3);
-    chThdSleepMilliseconds(500);
-  }
+        chThdSleepMilliseconds(500);
+    }
 }
 
 void nrf_dump_regs(nrf_regs *r) {
+    int i;
+    int j;
 
-  int i;
-  int j;
+    chprintf((BaseSequentialStream *) &SD1, "\n\r** START nRF2401 Register DUMP **\n\r");
 
-//	cio_print("\n\r** START nRF2401 Register DUMP **\n\r");
-  chprintf((BaseSequentialStream *) &SD1, "\n\r** START nRF2401 Register DUMP **\n\r");
+    nrf_reg_buf buf;
 
-  nrf_reg_buf buf;
+    for(i = 0; i < r->count; i++) {
 
-  for(i = 0; i < r->count; i++) {
+        nrf_read_reg(i, &buf);
 
-    nrf_read_reg(i, &buf);
+        if(r->data[i].size == 0) continue;
 
-    if(r->data[i].size == 0) continue;
+        chprintf((BaseSequentialStream *) &SD1, "%s: ", r->data[i].name);
 
-//		cio_printf("%s: ", r->data[i].name);
-    chprintf((BaseSequentialStream *) &SD1, "%s: ", r->data[i].name);
+        for(j = 0; j < buf.size; j++) {
+            chprintf((BaseSequentialStream *) &SD1, " (%u) ", buf.data[j]);
+        }
 
-    for(j = 0; j < buf.size; j++) {
-//			cio_printb(buf.data[j], 8);
-//			cio_printf(" (%u) ", buf.data[j]);
-      chprintf((BaseSequentialStream *) &SD1, " (%u) ", buf.data[j]);
+        chprintf((BaseSequentialStream *) &SD1,"\n\r - ");
+
+        for(j = 0; j < r->data[i].fields->count; j++) {
+            chprintf((BaseSequentialStream *) &SD1, "%u[%u]:%s=%u ", j,
+                     r->data[i].fields->data[j].size,
+                     r->data[i].fields->data[j].name,
+                     nrf_get_reg_field(i, j, &buf));
+        }
+
+        chprintf((BaseSequentialStream *) &SD1,"\n\r - ");
     }
-
-    chprintf((BaseSequentialStream *) &SD1,"\n\r - ");
-
-    for(j = 0; j < r->data[i].fields->count; j++) {
-      chprintf((BaseSequentialStream *) &SD1, "%u[%u]:%s=%u ", j,
-               r->data[i].fields->data[j].size,
-               r->data[i].fields->data[j].name,
-               nrf_get_reg_field(i, j, &buf));
-    }
-
-    chprintf((BaseSequentialStream *) &SD1,"\n\r - ");
-  }
-  chprintf((BaseSequentialStream *) &SD1,"** END **\n\r");
+    chprintf((BaseSequentialStream *) &SD1,"** END **\n\r");
 }
 
 /**
@@ -78,76 +73,76 @@ void nrf_dump_regs(nrf_regs *r) {
  */
 void nrf_configure_sb_tx(void) {
 
-  // Set address for TX and receive on P0
-  nrf_reg_buf addr;
+    // Set address for TX and receive on P0
+    nrf_reg_buf addr;
 
-  addr.data[0] = 1;
-  addr.data[1] = 2;
-  addr.data[2] = 3;
-  addr.data[3] = 4;
-  addr.data[4] = 5;
+    addr.data[0] = 1;
+    addr.data[1] = 2;
+    addr.data[2] = 3;
+    addr.data[3] = 4;
+    addr.data[4] = 5;
 
-  nrf_preset_sb(NRF_MODE_PTX, 40, 1, &addr);
+    nrf_preset_sb(NRF_MODE_PTX, 40, 1, &addr);
 
-  // Wait for radio to power up
-  chThdSleepMilliseconds(300);
+    // Wait for radio to power up
+    chThdSleepMilliseconds(300);
 }
 
 /*
  * Application entry point.
  */
 int main(void) {
-  unsigned i;
+    unsigned i;
 
-  /*
-   * System initializations.
-   * - HAL initialization, this also initializes the configured device drivers
-   *   and performs the board-specific initializations.
-   * - Kernel initialization, the main() function becomes a thread and the
-   *   RTOS is active.
-   */
-  halInit();
-  chSysInit();
+    /*
+     * System initializations.
+     * - HAL initialization, this also initializes the configured device drivers
+     *   and performs the board-specific initializations.
+     * - Kernel initialization, the main() function becomes a thread and the
+     *   RTOS is active.
+     */
+    halInit();
+    chSysInit();
 
-  /*
-   * SPI2 I/O pins setup.
-   */
-  palSetPadMode(GPIOB, 13, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* New SCK.     */
-  palSetPadMode(GPIOB, 14, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* New MISO.    */
-  palSetPadMode(GPIOB, 15, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* New MOSI.    */
-  palSetPadMode(GPIOB, 12, PAL_MODE_OUTPUT_PUSHPULL);              /* New CS.      */
+    /*
+     * SPI2 I/O pins setup.
+     */
+    palSetPadMode(GPIOB, 13, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* New SCK.     */
+    palSetPadMode(GPIOB, 14, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* New MISO.    */
+    palSetPadMode(GPIOB, 15, PAL_MODE_STM32_ALTERNATE_PUSHPULL);     /* New MOSI.    */
+    palSetPadMode(GPIOB, 12, PAL_MODE_OUTPUT_PUSHPULL);              /* New CS.      */
 
-  palSetPadMode(GPIOA, 9, PAL_MODE_STM32_ALTERNATE_PUSHPULL);      /* USART1 TX.       */
-  palSetPadMode(GPIOA, 10, PAL_MODE_INPUT);
+    palSetPadMode(GPIOA, 9, PAL_MODE_STM32_ALTERNATE_PUSHPULL);      /* USART1 TX.       */
+    palSetPadMode(GPIOA, 10, PAL_MODE_INPUT);
 
-  sdStart(&SD1, NULL);
+    sdStart(&SD1, NULL);
 
-  spiAcquireBus(&SPID2);              /* Acquire ownership of the bus.    */
-  spiStart(&SPID2, &ls_spicfg);       /* Setup transfer parameters.       */
+    spiAcquireBus(&SPID2);              /* Acquire ownership of the bus.    */
+    spiStart(&SPID2, &ls_spicfg);       /* Setup transfer parameters.       */
 
 //  nrf_init();
-  nrf_configure_sb_tx();
-  nrf_dump_regs(&nrf_reg_def);
+    nrf_configure_sb_tx();
+    nrf_dump_regs(&nrf_reg_def);
 
 // chThdCreateStatic(blinker_wa, sizeof(blinker_wa),
 //                    NORMALPRIO-1, blinker, NULL);
-  static nrf_payload   p;
+    static nrf_payload   p;
 
-  int s;
+    int s;
 
-  // set payload size according to the payload size configured for the RX channel
-  p.size = 1;
-  p.data[0] = 0;
+    // set payload size according to the payload size configured for the RX channel
+    p.size = 1;
+    p.data[0] = 0;
 
-  while (TRUE) {
-    chprintf((BaseSequentialStream *) &SD1, "Sending payload: %x ", p.data[0]);
-    s = nrf_send_blocking(&p);
-    chprintf((BaseSequentialStream *) &SD1, " - done; bytes send: %u\n\r", s);
+    while (TRUE) {
+        chprintf((BaseSequentialStream *) &SD1, "Sending payload: %x ", p.data[0]);
+        s = nrf_send_blocking(&p);
+        chprintf((BaseSequentialStream *) &SD1, " - done; bytes send: %u\n\r", s);
 
-    chThdSleepMilliseconds(100);
+        chThdSleepMilliseconds(100);
 
-    p.data[0]++;
-  }
+        p.data[0]++;
+    }
 
-  return 0;
+    return 0;
 }
